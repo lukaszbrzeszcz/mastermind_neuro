ceeef7ff22a2943332d78fdb072db0b76b37f6fd
Kamil <kam.popczyk@gmail.com>
2019-11-17 19:11:44 +0100

    New nn output.

--- ai/bot.py
+++ ai/bot.py
@@ -6,8 +6,9 @@ class Bot:
     def __init__(self, col, rows, input_range, brain=None):
         if brain:
             self.brain = brain
+            self.input_range = input_range
         else:
-            self.brain = nn.build_model(col * 2 * rows, input_range * rows)
+            self.brain = nn.build_model(col * 2 * rows, rows)
         self.input_range = input_range
         self.score = 0
         # output:
@@ -20,28 +21,8 @@ class Bot:
         game_board = np.concatenate((board, hints_board))
         nn_input = np.resize(game_board, (1, game_board.size))
         prediction_result = list(self.brain.predict(nn_input)[0])
-        position_1 = (
-            prediction_result[0 : self.input_range].index(
-                max(prediction_result[0 : self.input_range])
-            )
-            + 1
-        )
-        position_2 = (
-            prediction_result[self.input_range : self.input_range * 2].index(
-                max(prediction_result[self.input_range : self.input_range * 2])
-            )
-            + 1
-        )
-        position_3 = (
-            prediction_result[self.input_range * 2 : self.input_range * 3].index(
-                max(prediction_result[self.input_range * 2 : self.input_range * 3])
-            )
-            + 1
-        )
-        position_4 = (
-            prediction_result[self.input_range * 3 : self.input_range * 4].index(
-                max(prediction_result[self.input_range * 3 : self.input_range * 4])
-            )
-            + 1
-        )
+        position_1 = round(prediction_result[0]*self.input_range)
+        position_2 = round(prediction_result[1]*self.input_range)
+        position_3 = round(prediction_result[2]*self.input_range)
+        position_4 = round(prediction_result[3]*self.input_range)
         return [position_1, position_2, position_3, position_4]
